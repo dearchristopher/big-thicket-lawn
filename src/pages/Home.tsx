dcd45ced8153686679f0ef0bb0fb469a58ca8f70
Chris Roach <chris.roach@fsgi.com>
2025-07-29 17:58:22 -0500

    more stuff

---
+++ src/pages/Home.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="p-6">
+      <h1 className="text-4xl font-bold text-green-800">Big Thicket Lawn Service</h1>
+      <p className="mt-2 text-lg">Proudly serving Lumberton, Texas</p>
+    </div>
+  );
+}
