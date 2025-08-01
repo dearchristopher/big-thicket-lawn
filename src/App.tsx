d6da8df685bc1adc1d27c10dafb111f03bca2517
Chris Roach <chris.roach@fsgi.com>
2025-08-01 08:56:55 -0500

    add analytics

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import { Header } from "./components";
 import { Footer } from "./components/Footer";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App() {
   return (
     <div className="min-h-screen bg-green-50">
+      <Analytics />
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
