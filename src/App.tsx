1cb21b475257aec14b4e74ce45ed9004480e429c
Chris Roach <chris.roach@fsgi.com>
2025-08-01 08:59:12 -0500

    speed insights

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import Home from "./pages/Home";
 import { Header } from "./components";
 import { Footer } from "./components/Footer";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 export default function App() {
   return (
     <div className="min-h-screen bg-green-50">
       <Analytics />
+      <SpeedInsights />
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
